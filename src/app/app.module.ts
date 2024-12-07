8d9815ab56954d677b08893d3742b72ce392354e
viaanneyyovo <vianney.yovo@epitech.eu>
2024-12-07 09:57:44 +0000

    [FIX]: Resolved conflict in app-routing.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     // HeroDetailComponent,
-    // HeroesComponent
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    // AppRoutingModule,
     FormsModule,
-    HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,34 +1,69 @@
-import {Component} from '@angular/core';
-import {
-  NgIf,
-  NgFor,
-  UpperCasePipe,
-} from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
-import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
-import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
-  standalone: true,
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
-  imports: [
-    FormsModule,
-    // NgIf,
-    NgFor,
-    // UpperCasePipe,
-    HeroDetailComponent
-],
+  styleUrls: ['./heroes.component.css']
 })
+export class HeroesComponent implements OnInit {
 
-export class HeroesComponent {
-  heroes = HEROES;
   selectedHero?: Hero;
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
+
+
+// import {Component} from '@angular/core';
+// import {
+//   NgIf,
+//   NgFor,
+//   UpperCasePipe,
+// } from '@angular/common';
+// import {FormsModule} from '@angular/forms';
+
+// import {Hero} from '../hero';
+// import {HEROES} from '../mock-heroes';
+// import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
+
+// @Component({
+//   standalone: true,
+//   selector: 'app-heroes',
+//   templateUrl: './heroes.component.html',
+//   styleUrls: ['./heroes.component.css'],
+//   imports: [
+//     FormsModule,
+//     // NgIf,
+//     NgFor,
+//     // UpperCasePipe,
+//     HeroDetailComponent
+// ],
+// })
+
+// export class HeroesComponent {
+//   heroes = HEROES;
+//   selectedHero?: Hero;
+
+//   onSelect(hero: Hero): void {
+//     this.selectedHero = hero;
+//   }
+// }
