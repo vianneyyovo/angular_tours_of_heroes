c7aea5178c0a907d0963c8b4bcc0634eae167116
viaanneyyovo <vianney.yovo@epitech.eu>
2024-12-07 09:21:13 +0000

    [ADD]: step 3

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    // HeroDetailComponent,
+    // HeroesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HeroesComponent,
+    HeroDetailComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,34 @@
+import {Component} from '@angular/core';
+import {
+  NgIf,
+  NgFor,
+  UpperCasePipe,
+} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
+import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
+import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
+
+@Component({
+  standalone: true,
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+  imports: [
+    FormsModule,
+    // NgIf,
+    NgFor,
+    // UpperCasePipe,
+    HeroDetailComponent
+],
+})
+
+export class HeroesComponent {
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
