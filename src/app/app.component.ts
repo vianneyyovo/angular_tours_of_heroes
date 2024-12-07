9a4bc6c0b4e128b72ee6bf9a4d0228dbf31d8e81
viaanneyyovo <vianney.yovo@epitech.eu>
2024-12-07 14:12:35 +0000

    [ADD]: build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,3 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Tour of Heroes';
 }
-
-// import { Component } from '@angular/core';
-
-// @Component({
-//   standalone: false,
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   title = 'Tour of Heroes';
-// }
